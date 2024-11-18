1c969786d141975c896013f67d4d07b822c92ab8
Jha <janand@netapp.com>
2024-11-19 05:26:56 +0530

    Created new java file

---
+++ FlowOfControl.java
@@ -0,0 +1,9 @@
+public class FlowOfControl {
+
+    public static void main(String args[]){
+        int a = 40;
+        int b = 30;
+        int result = a + b;
+        System.out.println("The addition is "+result);
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Cloudswim
 Journey to Cloud
 Starting of the Project
+changes in the local
